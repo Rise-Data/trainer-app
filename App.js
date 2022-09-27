4e1f3dd972b75d0447736c7096ef229ecc7a2f98
mftech <matheussilvafeitosa78@gmail.com>
2022-09-26 22:40:19 -0300

    Criação da tela de alunos e adição de novos icones

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import Login from './components/Login/Login';
 import Profile from './components/Profile/Profile';
 import Register from './components/Register/Register';
 import SettingsAccount from './components/SettingsAccount/SettingsAccount';
+import Members from './components/Members/Members';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 const Stack = createNativeStackNavigator();
@@ -14,6 +15,7 @@ export default function App() {
         <Stack.Screen name="Register" component={Register} options={{headerShown: false}} />
         <Stack.Screen name="Profile" component={Profile} options={{ headerShown: false }}/>       
         <Stack.Screen name="SettingsAccount" component={SettingsAccount} options={{headerShown: false}} />
+        <Stack.Screen name="Members" component={Members} options={{headerShown: false}} />
       </Stack.Navigator>
     </NavigationContainer>
   );
